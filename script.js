fb3d44e99a56f8f1b0bb16784555de9ba88fe7a5
local-host-poc <73108978+local-host-poc@users.noreply.github.com>
2024-11-10 19:28:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-window.ui = SwaggerUIBundle({
-  url: 'https://app.atera.com/swagger/docs/v3',
-  dom_id: '#swagger-ui',
-  requestInterceptor: function (req) {
-    console.log('Request Headers:', req.headers);
-    if (req.headers['X-Api-Key']) {
-      console.log('X-Api-Key:', req.headers['X-Api-Key']);
+<div id="swagger-ui"></div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.52.5/swagger-ui-bundle.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.52.5/swagger-ui-standalone-preset.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.52.5/swagger-ui.css">
+
+<script>
+  window.ui = SwaggerUIBundle({
+    url: 'https://app.atera.com/swagger/docs/v3',
+    dom_id: '#swagger-ui',
+    requestInterceptor: function (req) {
+      console.log('Request Headers:', req.headers);
+      return req;
     }
-    return req;
-  }
-});
+  });
+</script>
