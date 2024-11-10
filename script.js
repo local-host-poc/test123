e71e8afdd38a45306666617060c28ea40874940d
local-host-poc <73108978+local-host-poc@users.noreply.github.com>
2024-11-10 19:26:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-fetch('https://app.atera.com/swagger/docs/v3', {
-  method: 'GET',
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest'  // Optional: sometimes helps bypass certain restrictions.
-  },
-  credentials: 'include'  // This allows cookies to be sent with the request.
-})
-.then(response => {
-  // Check if the response includes headers (depending on CORS policies).
-  for (let [key, value] of response.headers.entries()) {
-    if (key.toLowerCase() === 'x-api-key') {
-      console.log('X-Api-Key:', value);
-    } else {
-      console.log("NOT FOUND!");
+window.ui = SwaggerUIBundle({
+  url: 'https://app.atera.com/swagger/docs/v3',
+  dom_id: '#swagger-ui',
+  requestInterceptor: function (req) {
+    console.log('Request Headers:', req.headers);
+    if (req.headers['X-Api-Key']) {
+      console.log('X-Api-Key:', req.headers['X-Api-Key']);
     }
+    return req;
   }
-})
-.catch(error => console.error('Error:', error));
+});
