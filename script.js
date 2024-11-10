eeb9a2884f1da343dbf1ad74abe0e47c9110e48d
local-host-poc <73108978+local-host-poc@users.noreply.github.com>
2024-11-10 19:29:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let script = document.createElement('script');
 script.src = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.52.5/swagger-ui-bundle.js";
 script.onload = function() {
-  SwaggerUIBundle({
-    url: 'https://app.atera.com/swagger/docs/v3',
-    dom_id: '#swagger-ui',
-    requestInterceptor: function (req) {
-      console.log('Request Headers:', req.headers);
-      return req;
-    }
-  });
+  // Your Swagger UI configuration here
+  if (typeof SwaggerUIBundle !== 'undefined') {
+    SwaggerUIBundle({
+      url: 'https://app.atera.com/swagger/docs/v3',
+      dom_id: '#swagger-ui',
+      requestInterceptor: function (req) {
+        console.log('Request Headers:', req.headers);
+        return req;
+      }
+    });
+  } else {
+    console.log("SwaggerUIBundle still unavailable");
+  }
 };
 document.head.appendChild(script);
